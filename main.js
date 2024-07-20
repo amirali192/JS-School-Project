d5720909782fd59d6c21e834852a63bd3deef3c5
amirali192 <126576706+amirali192@users.noreply.github.com>
2024-07-20 17:34:14 +0330

    Uploaded the main files
    these are the main files of the project

---
+++ main.js
@@ -0,0 +1,102 @@
+let school = {
+
+    classA: {
+
+        S1CA: {
+
+            firstName: "Amirali",
+            lastName: "Chahbejari",
+            phone: "09172386646",
+            address: "bushehr",
+            grade: "15.69"
+
+        },
+        S2CA: {
+
+            firstName: "Daniel",
+            lastName: "Jalali",
+            phone: "Unknown",
+            address: "bushehr",
+            grade: "17"
+
+        },
+        S3CA: {
+
+            firstName: "MohammadReza",
+            lastName: "Unknown",
+            phone: "Unknown",
+            address: "bushehr",
+            grade: "9.75"
+
+        }
+    },
+
+    classB: {
+
+        S1CB: {
+
+            firstName: "Hadi",
+            lastName: "Bahadori",
+            phone: "09363241408",
+            address: "Borazjan",
+            grade: "19"
+
+        },
+        S2CB: {
+
+            firstName: "Reza",
+            lastName: "Salimi",
+            phone: "09385514036",
+            address: "bushehr",
+            grade: "14"
+
+        },
+        S3CB: {
+
+            firstName: "SpongeBob",
+            lastName: "SquarePants",
+            phone: "Unknown",
+            address: "USA",
+            grade: "16"
+
+        }
+    }
+};
+console.log("-------------------Student-1-Class-A :-------------------")
+console.log("S1CA-FirstName:", school.classA.S1CA.firstName)
+console.log("S1CA-LastName:", school.classA.S1CA.lastName)
+console.log("S1CA-PhoneNumber:", school.classA.S1CA.phone)
+console.log("S1CA-Address:", school.classA.S1CA.address)
+console.log("S1CA-Grade:", school.classA.S1CA.grade)
+console.log("-------------------Student-2-Class-A :-------------------")
+console.log("S2CA-FirstName:", school.classA.S2CA.firstName)
+console.log("S2CA-LastName:", school.classA.S2CA.lastName)
+console.log("S2CA-PhoneNumber:", school.classA.S2CA.phone)
+console.log("S2CA-Address:", school.classA.S2CA.address)
+console.log("S2CA-Grade:", school.classA.S2CA.grade)
+console.log("-------------------Student-3-Class-A :-------------------")
+console.log("S3CA-FirstName:", school.classA.S3CA.firstName)
+console.log("S3CA-LastName:", school.classA.S3CA.lastName)
+console.log("S3CA-PhoneNumber:", school.classA.S3CA.phone)
+console.log("S3CA-Address:", school.classA.S3CA.address)
+console.log("S3CA-Grade:", school.classA.S3CA.grade)
+console.log("-------------------Student-1-Class-B :-------------------")
+console.log("S3CA-FirstName:", school.classB.S1CB.firstName)
+console.log("S3CA-LastName:", school.classB.S1CB.lastName)
+console.log("S3CA-PhoneNumber:", school.classB.S1CB.phone)
+console.log("S3CA-Address:", school.classB.S1CB.address)
+console.log("S3CA-Grade:", school.classB.S1CB.grade)
+console.log("-------------------Student-2-Class-B :-------------------")
+console.log("S3CA-FirstName:", school.classB.S2CB.firstName)
+console.log("S3CA-LastName:", school.classB.S2CB.lastName)
+console.log("S3CA-PhoneNumber:", school.classB.S2CB.phone)
+console.log("S3CA-Address:", school.classB.S2CB.address)
+console.log("S3CA-Grade:", school.classB.S2CB.grade)
+console.log("-------------------Student-3-Class-B :-------------------")
+console.log("S3CA-FirstName:", school.classB.S3CB.firstName)
+console.log("S3CA-LastName:", school.classB.S3CB.lastName)
+console.log("S3CA-PhoneNumber:", school.classB.S3CB.phone)
+console.log("S3CA-Address:", school.classB.S3CB.address)
+console.log("S3CA-Grade:", school.classB.S3CB.grade)
+
+
